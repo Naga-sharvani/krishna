8669e80d735e7aa501c1b00b67b49a1b4ee9cbde
Naga-sharvani <pnagasharvani@gmail.com>
2026-02-02 21:48:49 +0530

    added the fake cursor and improved images

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+let mouseX=0,mouseY=0;
+let currX=0, currY=0;
+
 const btn=document.getElementById("krishna");
+const cursor=document.getElementById("fake");
 
 // “e is the event object provided by the browser that contains contextual information about the event. clientX and clientY give the mouse position relative to the viewport, which is useful for proximity-based UI behavior.”
 
 btn.addEventListener("mousemove",(e)=>{
     const rect=btn.getBoundingClientRect();
 
-    const x= e.clientX-rect.left;
-    const y= e.clientY-rect.top;
+    // const x= e.clientX-rect.left;
+    // const y= e.clientY-rect.top;
 
-    const dist= Math.sqrt(Math.pow(x,2) +Math.pow(y,2));
-    if(dist<100){
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const dx = x - rect.width / 2;
+    const dy = y - rect.height / 2;
+
+    const dist= Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+    if(dist<50){
         run();
     }
 });
 
 function run(){
-    const maxX= 1000-btn.offsetWidth;
-    const maxY = 400 -btn.offsetHeight;
+    const maxX= window.innerWidth-btn.offsetWidth;
+    const maxY = window.innerHeight-btn.offsetHeight;
 
     const randX= Math.random()*maxX;
     const randY= Math.random()*maxY;
@@ -24,3 +34,20 @@ function run(){
     btn.style.left = `${randX}px`;
     btn.style.top = `${randY}px`;
 }
+
+document.addEventListener("mousemove",(e)=>{
+    mouseX=e.clientX;
+    mouseY=e.clientY;
+})
+
+function moving(){
+    currX+=(-currX+mouseX)*0.1;
+    currY+=(-currY+mouseY)*0.1;
+
+    cursor.style.left=`${currX}px`;
+    cursor.style.top=`${currY}px`;
+
+    requestAnimationFrame(moving);
+}
+
+moving();
