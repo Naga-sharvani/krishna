dd6b9dccd48cfe991a75411cfe1e422954ee61ba
Naga-sharvani <pnagasharvani@gmail.com>
2026-02-12 22:52:35 +0530

    ig completed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let mouseX=0,mouseY=0;
 let currX=0, currY=0;
-
 const btn=document.getElementById("krishna");
 const cursor=document.getElementById("fake");
-
+let won=false;
 // “e is the event object provided by the browser that contains contextual information about the event. clientX and clientY give the mouse position relative to the viewport, which is useful for proximity-based UI behavior.”
 
 btn.addEventListener("mousemove",(e)=>{
+    if(won) return;
     const rect=btn.getBoundingClientRect();
-
     // const x= e.clientX-rect.left;
     // const y= e.clientY-rect.top;
 
@@ -25,6 +24,7 @@ btn.addEventListener("mousemove",(e)=>{
 });
 
 function run(){
+    if(won) return;
     const maxX= window.innerWidth-btn.offsetWidth;
     const maxY = window.innerHeight-btn.offsetHeight;
 
@@ -41,6 +41,7 @@ document.addEventListener("mousemove",(e)=>{
 })
 
 function moving(){
+    if(won) return;
     currX+=(-currX+mouseX)*0.1;
     currY+=(-currY+mouseY)*0.1;
 
@@ -49,5 +50,25 @@ function moving(){
 
     requestAnimationFrame(moving);
 }
+// const box = document.getElementById("box");
 
+
+document.addEventListener("mousemove", (e) => {
+  const rect = btn.getBoundingClientRect();
+  if(won) return;
+
+  if (
+    e.clientX >= rect.left &&
+    e.clientX <= rect.right &&
+    e.clientY >= rect.top &&
+    e.clientY <= rect.bottom
+  ) {
+    // console.log("Cursor collided with div!");
+    const win=document.createElement("div");
+    win.textContent="Haa Haa CAUGHT YOU..";
+    document.body.appendChild(win);
+    win.classList.add("win-message");
+    won=true;
+  }
+});
 moving();
